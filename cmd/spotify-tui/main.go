50b0d95b2b72f349efda8da1e068fc5e29c8107d
muiz-eddy <am.eddy25@gmail.com>
2024-11-26 06:26:30 +0800

    Implemented basic textInput using gocui

--- cmd/spotify-tui/main.go
+++ cmd/spotify-tui/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"github.com/jroimartin/gocui"
+	"log"
+	"spotify-tui/internal/spotifyUI"
+)
+
+func main() {
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer g.Close()
+
+	g.SetManagerFunc(spotifyUI.Layout)
+
+	g.Update(func(g *gocui.Gui) error {
+		_, err := g.SetCurrentView("input")
+		return err
+	})
+
+	if err := spotifyUI.Keybindings(g); err != nil {
+		log.Panicln(err)
+	}
+
+	// Start the GUI
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+}
--- internal/spotifyUI/searchInput.go
+++ internal/spotifyUI/searchInput.go
@@ -1,5 +1,46 @@
 package spotifyUI
 
 import (
+	"fmt"
+
 	"github.com/jroimartin/gocui"
 )
+
+func Layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+
+	if v, err := g.SetView("input", maxX/4, maxY/2-1, maxX*3/4, maxY/2+1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+
+		v.Title = "Input"
+		v.Editable = true
+		v.Wrap = true
+	}
+	return nil
+}
+
+func Keybindings(g *gocui.Gui) error {
+	if err := g.SetKeybinding("input", gocui.KeyEnter, gocui.ModNone, SubmitInput); err != nil {
+		return err
+	}
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		return err
+	}
+	return nil
+}
+
+func SubmitInput(g *gocui.Gui, v *gocui.View) error {
+	input := v.Buffer()
+	v.Clear()
+	v.SetCursor(0, 0)
+
+	fmt.Println("User Input: ", input)
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
+}
