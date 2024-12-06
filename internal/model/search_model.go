d849467578f0118c8444ac98f69e42efbd72e062
muiz-eddy <am.eddy25@gmail.com>
2024-12-07 01:00:11 +0800

    Added model structure

--- internal/model/search_model.go
+++ internal/model/search_model.go
@@ -8,3 +8,24 @@ type Tracks struct {
 
 type Album struct {
 }
+
+type Playlist struct{}
+
+type Shows struct{}
+
+type Items struct {
+	Id         string `json:"id"`
+	Popularity int8   `json:"popularity"`
+	Type       string `json:"type"`
+
+	Album struct {
+		Id          string `json:"id"`
+		AlbumType   string `json:"album_type"`
+		TotalTracks int8   `json:"total_tracks"`
+	}
+	Artist struct {
+		Id   string `json:"id"`
+		Name string `json:"name"`
+		Type string `json:"type"`
+	}
+}
