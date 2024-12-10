dad7532c3f85f23f4355b91ddeb7bcadb32d1779
muiz-eddy <am.eddy25@gmail.com>
2024-12-11 04:53:11 +0800

    Completed struct type (model) for spotify searching endpoint

--- internal/model/search_model.go
+++ internal/model/search_model.go
@@ -1,31 +1,66 @@
 package model
 
 type Search struct {
+	Tracks   `json:"tracks"`
+	Artists  `json:"artists"`
+	Albums   `json:"albums"`
+	Playlist `json:"playlist"`
+	Shows    `json:"shows"`
 }
 
 type Tracks struct {
+	Href     string  `json:"href"`
+	Previous *string `json:"previous"`
+	Next     *string `json:"next"`
+	Total    int     `json:"total"`
+	Items    []Items `json:"items"`
 }
 
-type Album struct {
+type Artists struct {
+	Href     string  `json:"href"`
+	Previous *string `json:"previous"`
+	Next     *string `json:"next"`
+	Total    int     `json:"total"`
+	Items    []Items `json:"items"`
+}
+type Albums struct {
+	Href     string  `json:"href"`
+	Previous *string `json:"previous"`
+	Next     *string `json:"next"`
+	Total    int     `json:"total"`
+	Items    []Items `json:"items"`
 }
 
-type Playlist struct{}
+type Playlist struct {
+	Href     string  `json:"href"`
+	Previous *string `json:"previous"`
+	Next     *string `json:"next"`
+	Total    int     `json:"total"`
+	Items    []Items `json:"items"`
+}
 
-type Shows struct{}
+type Shows struct {
+	Href     string  `json:"href"`
+	Previous *string `json:"previous"`
+	Next     *string `json:"next"`
+	Total    int     `json:"total"`
+	Items    []Items `json:"items"`
+}
 
 type Items struct {
-	Id         string `json:"id"`
-	Popularity int8   `json:"popularity"`
-	Type       string `json:"type"`
+	Id         *string `json:"id"`
+	Name       *string `json:"name"`
+	Popularity *int8   `json:"popularity"`
+	Type       *string `json:"type"`
 
 	Album struct {
-		Id          string `json:"id"`
-		AlbumType   string `json:"album_type"`
-		TotalTracks int8   `json:"total_tracks"`
+		Id          *string `json:"id"`
+		AlbumType   *string `json:"album_type"`
+		TotalTracks *int8   `json:"total_tracks"`
 	}
 	Artist struct {
-		Id   string `json:"id"`
-		Name string `json:"name"`
-		Type string `json:"type"`
+		Id   *string `json:"id"`
+		Name *string `json:"name"`
+		Type *string `json:"type"`
 	}
 }
